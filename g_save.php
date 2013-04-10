31d94d9711dd56a106863d7c79e9cfa1a7d9160b
sone4ko <_nastya_1993@mail.ru>
2013-04-10 15:52:54 +0300

    Create g_save.php
    Creating template for save php script (need to be corrected)

---
+++ g_save.php
@@ -0,0 +1,12 @@
+
+
+<?php
+$mebly=$_REQUEST['mebly'];
+echo "mebly=$mebly <br>";
+$tzvet=$_REQUEST['tzvet'];
+echo "tzvet=$tzvet <br>";
+$kilkist=$_REQUEST['kilkist'];
+echo "kilkist=$kilkist <br>";
+
+
+?>
