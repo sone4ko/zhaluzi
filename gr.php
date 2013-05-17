ec05b08cf159f7b28c9c11642944296a4d482e58
sone4ko <_nastya_1993@mail.ru>
2013-05-17 08:52:57 +0300

    Update gr.php

--- gr.php
+++ gr.php
@@ -5,9 +5,10 @@ $Vid=$_REQUEST['Vid'];
 echo "Vid=$Vid <br>";
 $kilkist=$_REQUEST['kilkist'];
 echo "kilkist=$kilkist <br>";
-
+$result=$_REQUEST['result'];
 echo "result=$result <br>";
 $fp=fopen("res.txt","a");
 fputs($fp,"$cvet\t$Vid\$kilkist\t$result\t");
 fclose($fp);
+
 ?>
