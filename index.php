a89c8eded77a51310e7eef01c6858b169a792d6e
sone4ko <_nastya_1993@mail.ru>
2013-05-25 19:19:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ header('Content-type: text/html; charset=utf-8');
 <title>Order value</title>
 </head>
 <body>
-<a href="g.php"> Калькулятор розрахунку вартості жалюзі</a>
+<a href="g.php"> Калькулятор розрахунку вартості жалюзі</a><br>
 <a href="11.php"> Перегляд замовлення</a>
 </body>
 </html>
